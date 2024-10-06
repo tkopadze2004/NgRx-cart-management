f5e0d2286cc61378067058fbde1ebe9893774e9e
Tamta <tamtakopadz16@gmail.com>
2024-10-06 22:36:02 +0400

    update spinner

--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -2,23 +2,27 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ItemComponent } from '../item/item.component';
 import { Store } from '@ngrx/store';
 import { selectFruits } from '../store/selectors/fruits.selector';
-import { map, Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { map, Observable, tap } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { SpinnerComponent } from "../spinner/spinner.component";
 
 @Component({
   selector: 'app-fruits',
   standalone: true,
-  imports: [ItemComponent, AsyncPipe],
+  imports: [ItemComponent, AsyncPipe, NgIf, SpinnerComponent],
   templateUrl: './fruits.component.html',
-  styleUrl: './fruits.component.scss',
+  styleUrls: ['./fruits.component.scss'],
 })
 export class FruitsComponent implements OnInit {
   private store = inject(Store);
-  public fruits$: Observable<unknown[]> | undefined;
+  public fruits$: Observable<any[]> | undefined;
+  public isLoading: boolean = true;
 
   public ngOnInit(): void {
-    this.fruits$ = this.store
-      .select(selectFruits)
-      .pipe(map((res) => res.fruits));
+    this.fruits$ = this.store.select(selectFruits).pipe(
+      tap(() => (this.isLoading = true)),  // Start the spinner
+      map((res) => res.fruits),
+      tap(() => (this.isLoading = false))
+    );
   }
 }
